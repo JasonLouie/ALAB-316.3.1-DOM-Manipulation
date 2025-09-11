066baf3ff1138f273cecc3c5b660dfca13881867
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-09-11 17:45:08 -0400

    Code refactoring (some functions need to be reused, others don't) and double checking comments.

--- script.js
+++ script.js
@@ -45,10 +45,11 @@ menuLinks.forEach(link => {
 
 // Part 2
 
-// Step 1 (Code refactor/ensuring everything is there)
-// Step 2 (Create nav with id of sub-menu add CSS):
+// Step 1: Getting Started (Code refactor/ensuring everything is there)
+// Step 2: Adding Additional HTML and CSS (Create nav with id of sub-menu in HTML file and add CSS)
 
-// Step 3
+
+// Step 3: Creating the Submenu
 // Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
 const subMenuEl = document.getElementById("sub-menu");
 
@@ -73,9 +74,13 @@ const topMenuLinks = topMenuEl.querySelectorAll("a");
 // Attach a delegated 'click' event listener to topMenuEl.
 topMenuEl.addEventListener("click", handleTopMenuClick);
 
+// Access h1 from mainEl
+const mainElH1 = mainEl.querySelector("h1");
+
 function handleTopMenuClick(e) {
     // The first line of code of the event listener function should call the event object's preventDefault() method.
     e.preventDefault();
+
     // The second line of code of the function should immediately return if the element clicked was not an <a> element.
     if (e.target.localName != "a") {
         return false;
@@ -83,25 +88,23 @@ function handleTopMenuClick(e) {
         // Log the content of the <a> to verify the handler is working.
         console.log(e.target.textContent);
         removeAllActiveExcept(e.target); // Remove all active classes from other <a> elements in topMenuLinks except for the target.
-        const object = getMenuObjectByName(e.target.textContent);
+        const object = getMenuObjectByName(e.target.textContent); // Get the specific object from the menuLinks array
 
         e.target.classList.toggle("active");
-        const mainElH1 = mainEl.querySelector("h1");
         if (e.target.classList.contains("active")) {
             removeSubmenu();
             if (object.subLinks) {
-                mainElH1.textContent = "DOM Manipulation"; // Set the h1 text back to DOM Manipulation
                 buildSubmenu(object.subLinks);
                 subMenuEl.style.top = "100%";
             } else { // If the ABOUT link is clicked, an <h1>About</h1> should be displayed.
-                mainElH1.textContent = object.text[0].toUpperCase() + object.text.slice(1);
+                mainElH1.textContent = capitalizeFirstLetters(object.text);
             }
         } else {
             subMenuEl.style.top = "0";
-            mainElH1.textContent = "DOM Manipulation"; // Set the h1 text back to DOM Manipulation
         }
     }
 
+    // Creates and appends child elements (anchor elements) for subMenuEl
     function buildSubmenu(subLinks) {
         for (const link of subLinks) {
             const anchorLink = document.createElement("a");
@@ -111,36 +114,31 @@ function handleTopMenuClick(e) {
         }
     }
 
-    
+    // Returns the object that has object.text === name
+    function getMenuObjectByName(name) {
+        for (let i = 0; i < menuLinks.length; i++) {
+            if (menuLinks[i].text === name) {
+                return menuLinks[i];
+            }
+        }
+    }
 
-    function removeSubmenu() {
-        const subMenuLinks = subMenuEl.querySelectorAll("a");
-        subMenuLinks.forEach(menuLink => {
-            subMenuEl.removeChild(menuLink);
-            subMenuEl.style.top = "0";
+    // Removes active tag from all anchor elements in the top menu except for the target
+    function removeAllActiveExcept(target) {
+        topMenuLinks.forEach(menuLink => {
+            if (menuLink.textContent != target.textContent) {
+                menuLink.classList.remove("active");
+            }
         });
     }
 }
 
-function getMenuObjectByName(name) {
-    for (let i = 0; i < menuLinks.length; i++) {
-        if (menuLinks[i].text === name) {
-            return menuLinks[i];
-        }
-    }
-}
-
-function removeAllActiveExcept(target) {
-    topMenuLinks.forEach(menuLink => {
-        if (menuLink.textContent != target.textContent) {
-            menuLink.classList.remove("active");
-        }
-    });
-}
-
-function removeAllActive() {
-    topMenuLinks.forEach(menuLink => {
-        menuLink.classList.remove("active");
+// Removes child elements (anchor elements) of subMenuEl
+function removeSubmenu() {
+    const subMenuLinks = subMenuEl.querySelectorAll("a");
+    subMenuLinks.forEach(menuLink => {
+        subMenuEl.removeChild(menuLink);
+        subMenuEl.style.top = "0";
     });
 }
 
@@ -165,16 +163,26 @@ function handleSubMenuClick(e) {
         removeAllActive();
 
         // Update the contents of mainEl, within an <h1>, to the contents of the <a> element clicked within subMenuEl.
-        const aboutH1 = mainEl.querySelector("h1");
         const subMenuText = e.target.textContent;
-        aboutH1.textContent = capitalizeFirstLetters(subMenuText);
+        mainElH1.textContent = capitalizeFirstLetters(subMenuText);
+
+        // Remove subMenuEl's anchor elements
+        removeSubmenu();
     }
 
-    function capitalizeFirstLetters(words){
-        const wordArray = words.split(" ");
-        for(let i = 0; i < wordArray.length; i++){
-            wordArray[i] = wordArray[i][0].toUpperCase() + wordArray[i].slice(1);
-        }
-        return wordArray.join(" ")
+    // Removes active class from all anchor elements in the top menu
+    function removeAllActive() {
+        topMenuLinks.forEach(menuLink => {
+            menuLink.classList.remove("active");
+        });
     }
+}
+
+// Capitalizes the first letters of each word
+function capitalizeFirstLetters(words) {
+    const wordArray = words.split(" ");
+    for (let i = 0; i < wordArray.length; i++) {
+        wordArray[i] = wordArray[i][0].toUpperCase() + wordArray[i].slice(1);
+    }
+    return wordArray.join(" ")
 }
